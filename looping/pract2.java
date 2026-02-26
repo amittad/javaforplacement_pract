665a16de0cc08ab771ff189037eaed9f0152a67f
amittad <tadamit2004@gmail.com>
2026-02-26 17:12:00 +0530

    Updated feature

---
+++ looping/pract2.java
@@ -0,0 +1,13 @@
+package looping;
+//. Print all even numbers between 1 and 100.
+
+public class pract2 {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
