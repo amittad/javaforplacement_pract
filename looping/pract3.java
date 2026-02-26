2ccb53fe7e37a0e5a41ff44bb39bb2bc4d00a77f
amittad <tadamit2004@gmail.com>
2026-02-26 17:22:56 +0530

    Updated feature

---
+++ looping/pract3.java
@@ -0,0 +1,18 @@
+package looping;
+//. Print all odd numbers between 1 and 100.
+public class pract3 {
+    public static void main(String[] args){
+       int i=1;
+       while (i<=100) {
+        if(i%2==1){
+            System.out.println(i);
+        }
+        i++;
+       }
+       
+
+
+    }
+
+
+}
