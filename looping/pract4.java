d20ab1fcf20d838eaf560066509dc45142c652a3
amittad <tadamit2004@gmail.com>
2026-02-26 17:29:47 +0530

    Updated feature

---
+++ looping/pract4.java
@@ -0,0 +1,11 @@
+package looping;
+//4. Print numbers from 10 down to 1. 
+
+public class pract4 {
+    public static void main(String[] args) {
+        for(int i=10;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+    
+}
