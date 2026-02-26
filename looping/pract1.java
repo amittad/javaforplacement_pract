d6f5f4f3b723b0d946643d494840fb99a61369f0
amittad <tadamit2004@gmail.com>
2026-02-26 17:09:15 +0530

    Updated feature

---
+++ looping/pract1.java
@@ -0,0 +1,12 @@
+//Print numbers from 1 to 10.
+ package looping;
+
+class pract1 {
+public static void main(String[] args) {
+    for(int i=1;i<=10;i++){
+        System.out.print(i);
+    }
+    
+}
+    
+}
