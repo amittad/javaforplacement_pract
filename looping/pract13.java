91074ff710c2ddceb179d57b43ec5a3db95a356d
amittad <tadamit2004@gmail.com>
2026-02-28 01:50:08 +0530

    first commit

---
+++ looping/pract13.java
@@ -0,0 +1,21 @@
+package looping;
+
+public class pract13 {
+    public static void main(String[] args) {
+        
+        for(int i =2;i<=100;i++){
+            boolean isprime=true;
+            for(int j=2;j<i;j++){
+                if(i%j==0) {
+                    isprime=false;
+                    break;
+                    
+                    
+                }
+            }
+            if (isprime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
