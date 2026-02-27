1ef1fb84bac8c1d14f946ff830a643f78b08a1a9
amittad <tadamit2004@gmail.com>
2026-02-28 01:41:34 +0530

    first commit

--- looping/pract12.java
+++ looping/pract12.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class pract12 {
     public static void main(String[] args) {
         int add=0;
-        Scanner sc =new Scanner("System.in");
+        Scanner sc =new Scanner(System.in );
         int num=sc.nextInt();
         int ba=num;
         for(int i=1;i<num;i++){
-            if(ba%num==0){
-                add+=num;
+            if(ba%i==0){
+                add+=i;
             }
         }
         if (add==ba) {
             System.out.println("this number is a perfect numer");
             
+        }
+        else{
+            System.out.println("this number is not perfect nummer");
         }
     }
     
