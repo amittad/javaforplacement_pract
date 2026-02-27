43f0c0ac8c088a1f15d0c0fbe5ce09c88829549a
amittad <tadamit2004@gmail.com>
2026-02-28 02:14:00 +0530

    first commit

---
+++ looping/pract15.java
@@ -0,0 +1,25 @@
+package looping;
+import java.util.Scanner;
+
+public class pract15 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        sc.close();
+    }
+}
