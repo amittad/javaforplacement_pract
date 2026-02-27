57141852b2a0a21673140f551b33e7da0c12d6d0
amittad <tadamit2004@gmail.com>
2026-02-28 00:33:45 +0530

    first commit

--- looping/pract10.java
+++ looping/pract10.java
@@ -6,7 +6,16 @@ public class pract10 {
 //. Find the sum of digits of a number. 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("hhh");
+        int num=sc.nextInt();
+        int sum=0;
+        int last=0;
+        while (num>0) {
+            last=num%10;
+            sum+=last;
+            num=num/10;
+            
+        }
+        System.out.println(sum);
 
         
     }
