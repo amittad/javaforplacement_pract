2ad6e92c848011e7810ecf43c46979a7e670cbdf
amittad <tadamit2004@gmail.com>
2026-03-09 00:14:51 +0530

    add array folder

---
+++ array/pract2.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Scanner;
+
+public class pract2 {
+     public static void main(String[] args) {
+        System.out.println("input the range");
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i=0;i<n;i++){
+            int inp=sc.nextInt();
+            arr[i]=inp;
+
+        }
+        for (int j =arr.length; j >0; j--) {
+
+            System.out.println(arr[j]);
+            
+        }
+
+    }
+    
+}
