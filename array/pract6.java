0561d48dc5739c8fdacf88fc7bdd2dc1367acce3
amittad <tadamit2004@gmail.com>
2026-03-10 01:17:04 +0530

    add praxt 6

--- array/pract6.java
+++ array/pract6.java
@@ -6,14 +6,17 @@ public class pract6 {
         int max1=arr[0];
         int min1=arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if(arr[i]>max){
+            if(arr[i]>max1){
                 max1=arr[i];
             }
-            else{
+            if(arr[i]<min1){
                 min1=arr[i];
             }
+           
             
         }
+
+       
         System.out.println(min1+""+max1);
     }
     
