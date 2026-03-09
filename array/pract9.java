d78f91d7cb65c9d6cf9b393096a34c04a123be14
amittad <tadamit2004@gmail.com>
2026-03-10 01:41:56 +0530

    add praxt 6

--- array/pract9.java
+++ array/pract9.java
@@ -4,16 +4,24 @@ public class pract9 {
     public static void main(String[] args) {
         int arr[]={1,22,4,67,34,98,76,0};
         int max=arr[0];
+        int min=arr[0];
         int add=0;
+        int addmin=0;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i]>max) {
                 max=arr[i];
                 add=i;
                 
             }
+            if (arr[i]<min) {
+                min=arr[i];
+                addmin=i;
+                
+            }
             
         }
         System.out.println(add);
+        System.out.println(addmin);
     }
     
 }
