d830f07fdad066b4b43c96031f1507d2dc8ce9a8
amittad <tadamit2004@gmail.com>
2026-03-10 01:12:28 +0530

    add praxt 6

--- array/pract6.java
+++ array/pract6.java
@@ -1,5 +1,19 @@
 package array;
 
 public class pract6 {
+    public static void main(String[] args) {
+        int arr[]={11,23,65,67,98,1,5};
+        int max=arr[0];
+        int min=arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            else{
+                min=arr[i];
+            }
+            
+        }
+    }
     
 }
