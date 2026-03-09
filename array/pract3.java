757ca7ed720cdca2c1efdd8599760afac3e9fced
amittad <tadamit2004@gmail.com>
2026-03-10 00:58:27 +0530

    add praxt 3

--- array/pract3.java
+++ array/pract3.java
@@ -2,7 +2,7 @@ package array;
 //2. Find the sum of all elements in an array. 
 public class pract3 {
     public static void main(String[] args) {
-        int arr[]={11,23,45,22};
+        int arr[]={11,23,4,5,6,6};
         int add=0;
         for (int i : arr) {
             add+=i;
